cc891d0dbdded66c1c73dec32924cb65c07bc199
James LaChance <fatboyxpc@gmail.com>
2019-02-25 03:49:45 -0500

    Add validations for require fields
    Note: Intentionally left validations out for the Passport fields as the
    stock passport stuff doesn't use a Laravel validator for it, thus for
    consistency the registration controller doesn't have it either.

--- tests/Feature/UserCanRegisterTest.php
+++ tests/Feature/UserCanRegisterTest.php
@@ -42,4 +42,131 @@ class UserCanRegisterTest extends TestCase
         $this->assertEquals('fatboyxpc@gmail.com', $user->email);
         $this->assertTrue(Auth::attempt(['email' => $user->email, 'password' => 'secret']));
     }
+
+    /** @test */
+    public function theseFieldsAreRequiredToRegister()
+    {
+        $response = $this->postJson('api/register');
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors([
+            'name',
+            'email',
+            'password',
+        ]);
+    }
+
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'name' => 'James',
+            'email' => 'fatboyxpc@gmail.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function nameMustBeString()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'name' => 123,
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('name');
+    }
+
+    /** @test */
+    public function nameMustNotBeGreaterThan255Characters()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'name' => str_random(256),
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('name');
+    }
+
+    /** @test */
+    public function emailMustBeString()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'email' => 123,
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+    }
+
+    /** @test */
+    public function emailMustBeValidEmailAddress()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'email' => 'fatboyxpc',
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+    }
+
+    /** @test */
+    public function emailMustNotBeGreaterThan255Characters()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'email' => str_random(256),
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+    }
+
+    /** @test */
+    public function emailMustBeUnique()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'fatboyxpc@gmail.com',
+        ]);
+
+        $response = $this->postJson('api/register', $this->validParams([
+            'email' => 'fatboyxpc@gmail.com',
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+    }
+
+    /** @test */
+    public function passwordMustBeString()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'password' => 123456,
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('password');
+    }
+
+    /** @test */
+    public function passwordMustNotBeLessThan6Characters()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'password' => str_random(5),
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('password');
+    }
+
+    /** @test */
+    public function passwordMustMatchConfirmation()
+    {
+        $response = $this->postJson('api/register', $this->validParams([
+            'password' => 'secret',
+            'password_confirmation' => 'secret2',
+        ]));
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('password');
+    }
 }
