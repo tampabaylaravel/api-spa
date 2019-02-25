a5226ce95fb5c3218027e71533cf629eaf879dbc
James LaChance <fatboyxpc@gmail.com>
2019-02-25 04:10:31 -0500

    Add Passport/League/Oauth validation checks

--- tests/Feature/UserCanLoginToApiTest.php
+++ tests/Feature/UserCanLoginToApiTest.php
@@ -12,6 +12,18 @@ class UserCanLoginToApiTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $oauthClient;
+
+    private $user;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->oauthClient = factory(Client::class)->states('password')->create();
+        $this->user = factory(User::class)->create();
+    }
+
     /** @test */
     public function userCanGetAccessTokenAfterLoggingIn()
     {
@@ -34,5 +46,84 @@ class UserCanLoginToApiTest extends TestCase
         $this->assertNotEmpty($json['expires_in']);
         $this->assertNotEmpty($json['access_token']);
         $this->assertNotEmpty($json['refresh_token']);
+    }
+
+    private function validOauthParams($overrides = [])
+    {
+        return array_merge([
+            'grant_type' => 'password',
+            'client_id' => $this->oauthClient->id,
+            'client_secret' => $this->oauthClient->secret,
+            'username' => $this->user->email,
+            'password' => 'secret',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function userCannotLoginWithoutValidGrantType()
+    {
+        $response = $this->postJson('api/auth/token', $this->validOauthParams([
+            'grant_type' => '',
+        ]));
+
+        $response->assertStatus(400);
+
+        $json = $response->json();
+        $this->assertEquals('unsupported_grant_type', $json['error']);
+    }
+
+    /** @test */
+    public function userCannotLoginWithoutValidClientId()
+    {
+        $response = $this->postJson('api/auth/token', $this->validOauthParams([
+            'client_id' => '',
+        ]));
+
+        $response->assertStatus(400);
+
+        $json = $response->json();
+        $this->assertEquals('invalid_request', $json['error']);
+        $this->assertContains('client_id', $json['hint']);
+    }
+
+    /** @test */
+    public function userCannotLoginWithoutValidClientSecret()
+    {
+        $response = $this->postJson('api/auth/token', $this->validOauthParams([
+            'client_secret' => '',
+        ]));
+
+        $response->assertStatus(401);
+
+        $json = $response->json();
+        $this->assertEquals('invalid_client', $json['error']);
+    }
+
+    /** @test */
+    public function userCannotLoginWithoutValidUser()
+    {
+        $response = $this->postJson('api/auth/token', $this->validOauthParams([
+            'username' => '',
+        ]));
+
+        $response->assertStatus(400);
+
+        $json = $response->json();
+        $this->assertEquals('invalid_request', $json['error']);
+        $this->assertContains('username', $json['hint']);
+    }
+
+    /** @test */
+    public function userCannotLoginWithoutPassword()
+    {
+        $response = $this->postJson('api/auth/token', $this->validOauthParams([
+            'password' => '',
+        ]));
+
+        $response->assertStatus(400);
+
+        $json = $response->json();
+        $this->assertEquals('invalid_request', $json['error']);
+        $this->assertContains('password', $json['hint']);
     }
 }
